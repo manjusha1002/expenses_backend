d7e752af231ca6f640c1af44f04983b541144fa6
Anand <ak7610052@gmail.com>
2025-05-03 15:59:33 +0530

    policy changed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
