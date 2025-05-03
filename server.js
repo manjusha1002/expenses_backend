aa4e753089e7151a4c102d2088264ef8511b55dd
Anand <ak7610052@gmail.com>
2025-05-03 15:50:34 +0530

    cors changed

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({
-    origin: 'https://expenses-frontend-manjusha1002s-projects.vercel.app/',
-    credentials: false
-}));
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
